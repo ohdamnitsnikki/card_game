3e692ddd2381bfc37d6211524a6481136059bc5e
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2022-11-29 00:22:54 +0000

    add function to game numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,37 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let button of buttons) {
         button.addEventListener("click", function(){
+            if let gameType = this.getAttribute("above"); {
+                runGame(gameType);
+                
+            } else {
+                let gameType = this.getAttribute("below");
+                runGame(gameType);
+            }
 
         })
     }
 
 })
 
-startGame() {
+/**
+ * The game loop that starts after the script is loaded
+ * and after the user pressed one of the buttons
+ */
+
+function startGame(gameType) {
 
     //creates two whole random numbers between 1-14
     let num1 = Math.floor(Math.random() * 14) +1;
     let num2 = Math.floor(Math.random() * 14) +1;
 }
 
-checkAnswer()
+function checkAnswer()
 
-checkCorrectAnswer()
+function checkCorrectAnswer()
 
-increaseWictory()
+function increaseWictory()
 
-increaseDefeat()
+function increaseDefeat()
+
+function giveCard(leftCard)
